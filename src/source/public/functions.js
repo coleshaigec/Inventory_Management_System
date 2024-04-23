4b0f904f421e8be9beb34bd09abb1f8c029b07b4
Vugar Amirov <vamirov@bu.edu>
2024-04-23 19:48:01 -0400

    Update functions.js

--- src/source/public/functions.js
+++ src/source/public/functions.js
@@ -1,24 +1,51 @@
 const { ipcRenderer } = require('electron');
 
-// document.getElementById('loginForm').addEventListener('submit', (event) => {
-//     event.preventDefault();
-//     const username = document.getElementById('username').value;
-//     const password = document.getElementById('password').value;
+document.addEventListener('DOMContentLoaded', () => {
+    	const loginForm = document.getElementById('loginForm');
+    	const registerForm = document.getElementById('registerForm');
+	
+	if (loginForm) {
+        	loginForm.addEventListener('submit', (event) => {
+            		event.preventDefault();
+            		const username = document.getElementById('username').value;
+            		const password = document.getElementById('password').value;
+            		ipcRenderer.send('perform-login', { username, password });
+        	});
+	}
 
-//     ipcRenderer.send('perform-login', { username, password });
-//   });
-  
-ipcRenderer.on('login-success', (event, arg) => {
-    window.location.href = '../views/map.html';
-    remote.app.emit('map-page');
-   });
+    	if (registerForm) {
+        	registerForm.addEventListener('submit', (event) => {
+            		event.preventDefault();
+            		const username = document.getElementById('username').value;
+            		const password = document.getElementById('password').value;
+                        const status = document.getElementById('status').value;
+            		ipcRenderer.send('perform-register', { username, password, status });
+        	});
+	}
+
+	const goToLoginButton = document.getElementById('goToLogin');
+    	if (goToLoginButton) {
+        	goToLoginButton.addEventListener('click', () => {
+            		window.location.href = 'login.html';
+        	});
+    	}
+
+    	const goToRegisterButton = document.getElementById('goToRegister');
+    	if (goToRegisterButton) {
+        	goToRegisterButton.addEventListener('click', () => {
+            		window.location.href = 'register.html';
+        	});
+    	}
+
+});
 
 ipcRenderer.on('login-failure', (event, data) => {
-    if (data.attemptsLeft > 0) {
-        document.getElementById('message').textContent = `Login failed. Attempts left: ${data.attemptsLeft}`;
-    } else {
-        document.getElementById('message').textContent = 'You have exceeded the number of login attempts.';
-    }
+	document.getElementById('message').textContent = `Login failed. Attempts left: ${data.attemptsLeft}`;
+});
+
+
+ipcRenderer.on('register-failure', (event, data) => {
+    	document.getElementById('message_register').textContent = data.registerResponse;
 });
 
  const box = $(".single-notification-box");
@@ -35,63 +62,3 @@ ipcRenderer.on('login-failure', (event, data) => {
          unreadNotificationNumber.text("0");
      }
  });
-
-// function runPythonScript(scriptPath: string = "../LoginSystem.py", username, password) {
-//     const python = spawn('python', [scriptPath, ...args]);
-
-//     python.stdout.on('data', (data) => {
-//         console.log(stdout: ${data});
-//     });
-
-//     python.stderr.on('data', (data) => {
-//         console.error(stderr: ${data});
-//     });
-
-//     python.on('close', (code) => {
-//         console.log(child process exited with code ${code});
-//     });
-// }
-
-ipcRenderer.on('login-attempt-exceeded', () => {
-    document.getElementById('message').textContent = 'You have exceeded the number of login attempts.';
-    window.location.href = '../views/index.html';
-    remote.app.emit('main-page');
-});
-
-ipcRenderer.on('reset-login', (event,arg) => {
-    document.getElementById('message').textContent = '';
-    // Optionally, clear the username and password fields
-    document.getElementById('username').value = '';
-    document.getElementById('password').value = '';
-    // Enable the login button if it was previously disabled
-    document.getElementById('loginButton').disabled = false;
-});
-
-// document.getElementById('registerForm').addEventListener('submit', (event) => {
-//     event.preventDefault();
-//     const username = document.getElementById('username').value;
-//     const password = document.getElementById('password').value;
-//     const status = document.getElementById('status').value;
-
-//     ipcRenderer.send('perform-register', { username, password, status });
-//   });
-
-ipcRenderer.on('register_success', (event,data) => {
-    window.location.href = '../views/index.html';
-    remote.app.emit('main-page');
-});
-
-ipcRenderer.on('register-failure', (event, data) => {
-    const our_data = data.response;
-    document.getElementById('message_register').textContent = our_data;
-});
-
-ipcRenderer.on('reset-register', () => {
-    document.getElementById('message').textContent = '';
-    // Optionally, clear the username and password fields
-    document.getElementById('username').value = '';
-    document.getElementById('password').value = '';
-    document.getElementById('status').value = '';
-    // Enable the login button if it was previously disabled
-    document.getElementById('loginButton').disabled = false;
-});
