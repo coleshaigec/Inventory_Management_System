92e5844229f80631b43cf869fc4b7cf88a8f745c
Vugar Amirov <vamirov@bu.edu>
2024-04-23 19:50:20 -0400

    Update LoginSystem.py

--- src/database/LoginSystem.py
+++ src/database/LoginSystem.py
@@ -16,50 +16,14 @@ letters = ('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','
 capitals = ('A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')
 numbers = (1,2,3,4,5,6,7,8,9,0)
 
-# TALK TO PREE ABOUT THE USER ACCESS LEVEL
-# TALK TO PREE ABOUT THE RETURNS AND PRINTS
-
-
-
-
 class LoginSystem:
     
-    # Initialiser for LoginSystem class
     def __init__(self):
-        
-        # Connects to the BusinessInventoryChecker database
         URI = "mongodb+srv://" + "Admin" + ":" + "Admin" + "@businessinventorychecke.hnarzhd.mongodb.net/?retryWrites=true&w=majority&appName=BusinessInventoryChecker&tlsInsecure=true"
         client = MongoClient(URI, server_api=ServerApi('1'))
-        
-        # Assign the AccessDetails collection from LoginSystem database to variable called login_DB 
         self.data_base = client['LoginSystem']
         self.login_DB = self.data_base['AccessDetails']
-      
-      
-      
-      
-      
-    # First stage of login asking the user for either creating an account or logging in
-    def start_login(self):
-        
-        # Ask the user if they have an account
-        choice = input("Do you have an account (yes or no): ")
-        
-        # Keep asking the same question until the user inputs yes or no
-        while choice != "yes" and choice != "no":
-            choice = input("Please enter either yes or no: ")
-            
-        # if user has account, then the functions called the login function, else it calls the register function
-        if choice == "yes":
-            return self.login()
-        else:
-            return self.register()
-    
-    
-    
-    
-    
-    # logging in the user
+
     def login(self, username, password):
         user_check = self.login_DB.find_one({'username': username})
         if user_check:    
@@ -77,9 +41,6 @@ class LoginSystem:
             print("Wrong Password")
             return False
             
-            
-            
-    
     def register(self, username, password, status):
         user_check = self.login_DB.find_one({'username': username})
         case = False
@@ -109,62 +70,38 @@ class LoginSystem:
             if capital == False:
                 print("Need Capitals!")
                 case = True
+        admin_user = self.login_DB.find_one({'status': 'Admin'})
+        if status == "ReadWrite":
+            self.status = "ReadWrite"
+        elif status == "Admin":
+            self.status = "Admin"
+        elif status == "Read":
+            self.status ="Read"
         else:
-            admin_user = self.login_DB.find_one({'status': 'Admin'})
-            if status == "ReadWrite":
-                self.status = "RW"
-            elif status == "Admin":
-                self.status = "Admin"
-            elif status == "Read":
-                self.status ="R"
-            else:
-                print("Wrong Status!")
-                case = True
-            if case:
-                return False
-            else:
-                self.username = username
-                self.status = status
-                self.fiscal_year = admin_user.get('fiscal_year')
-                self.lifo_fifo = admin_user.get('lifo_fifo')
-                hashed_password = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
-                self.login_DB.insert_one({'username': username, 'password': hashed_password, 'status': status, 'fiscal_year': self.fiscal_year, 'lifo_fifo': self.lifo_fifo})
-                print("Success")
-                return True
-    
-    
-    
-    
-    
-    # change the fiscal year for everyone, only admins can do it
-    def change_fiscal_year(self):
-        
-        # Checks if the user is admin and if not, prints the correct message
-        if self.status != "Admin":
-            print("You can not change the fiscal year")
+            print("Wrong Status!")
+            case = True
+        if case:
             return False
-        
-        # gets a new fiscal year date and changes the database for all the users accordingly
-        new_fiscal_year = HelperFunctions.check_fiscal_year()
-        result = self.login_DB.update_many({}, {"$set": {"fiscal_year": new_fiscal_year}})
-        return True
+        else:
+            self.username = username
+            self.status = status
+            self.fiscal_year = admin_user.get('fiscal_year')
+            self.lifo_fifo = admin_user.get('lifo_fifo')
+            hashed_password = bcrypt.hashpw(password.encode('utf8'), bcrypt.gensalt())
+            self.login_DB.insert_one({'username': username, 'password': hashed_password, 'status': status, 'fiscal_year': self.fiscal_year, 'lifo_fifo': self.lifo_fifo})
+            print("Success")
+            return True
     
-    # change the lifo fifo computation for everyone, only admin can do it
-    def change_lifo_fifo(self):
-        
-        # Checks if the user is admin and if not, prints the correct message
-        if self.status != "Admin":
-            print("You can not change the life or fifo computation")
+    def change_fiscal_year(self, date, status):
+        if status != "Admin":
+            print("You can not change the fiscal year!")
             return False
-        
-        # gets a new fifo or lifo computation and changes the database for all the users accordingly
-        new_lifo_fifo = HelperFunctions.check_lifo_fifo()
-        result = self.login_DB.update_many({}, {"$set": {"lifo_fifo": new_lifo_fifo}})
+        self.login_DB.update_many({}, {"$set": {"fiscal_year": date}})
         return True
 
-        
-        
-        
-        
-        
-        
+    def change_lifo_fifo(self, lifo_fifo, status):
+        if self.status != "Admin":
+            print("You can not change the life or fifo computation!")
+            return False
+        self.login_DB.update_many({}, {"$set": {"lifo_fifo": lifo_fifo}})
+        return True
